829627771f3d979901f9cab0d54e4df88e5bd803
furtadomn <mnfurtado@yahoo.com.br>
2022-07-18 12:41:20 -0300

    boutique inventory + tests

---
+++ lib/boutique_inventory.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class BoutiqueInventory
+  attr_reader :inventory
+
+  def initialize(inventory)
+    @inventory = inventory
+  end
+
+  def item_names
+    @inventory.map { |n| n[:name] }.sort
+  end
+
+  def cheap
+    cheap_items = []
+
+    @inventory.map do |p|
+      p[:price] < 30 ? cheap_items << p : next
+    end
+
+    cheap_items
+  end
+
+  def out_of_stock
+    no_stock = []
+
+    inventory.map do |stock|
+      stock[:quantity_by_size].empty? ? no_stock << stock : next
+    end
+
+    no_stock
+  end
+
+  def stock_for_item(name)
+    stock_for_item_name = inventory.find do |stock|
+      stock[:name] == name ? stock[:quantity_by_size] : next
+    end
+
+    stock_for_item_name[:quantity_by_size]
+  end
+
+  def total_stock
+    stock_array = inventory.map do |stock|
+      stock[:quantity_by_size].values.reduce(:+)
+    end
+
+    total = []
+    stock_array.map do |num|
+      num == nil ? next : total << num
+    end
+
+    total.empty? ? 0 : total.reduce(:+)
+  end
+end
---
+++ spec/boutique_inventory_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'boutique_inventory'
+
+RSpec.describe BoutiqueInventory do
+  let(:subject) { BoutiqueInventory.new(inventory) }
+
+  let(:inventory) do
+    [
+      {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+      {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+      {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+      {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+    ]
+  end
+
+  describe '.item_names' do
+    let(:array_item_names) { ["Bamboo Socks Cats", "Black Short Skirt", "Maxi Brown Dress", "Red Short Skirt"] }
+
+    it 'returns a list of the item names ordered alphabetically' do
+      expect(subject.item_names).to eq(array_item_names)
+    end
+  end
+
+  describe '.cheap' do
+    let(:cheap_items) do
+      [
+        {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+        {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+      ]
+    end
+
+    it 'returns any items that cost less than $30' do
+      expect(subject.cheap).to eq(cheap_items)
+    end
+  end
+
+  describe '.out_of_stock' do
+    let(:no_stock) { [{price: 50.00, name: "Red Short Skirt", quantity_by_size: {}}] }
+
+    it 'returns any items that have no stock' do
+      expect(subject.out_of_stock).to eq(no_stock)
+    end
+  end
+
+  describe '.stock_for_item' do
+    let(:name) { "Black Short Skirt" }
+
+    it 'returns the stock of an item by its name' do
+      expect(subject.stock_for_item(name)).to eq({ s: 1, xl: 4 })
+    end
+  end
+
+  describe '.total_stock' do
+    it 'calculates the total amount of items in your storeroom:' do
+      expect(subject.total_stock).to eq(36)
+    end
+  end
+end
